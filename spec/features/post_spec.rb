c4a2d21d69da07fef6782708cad2394a8fbcbfe6
Matt Rider <mattwr18@gmail.com>
2017-09-17 14:59:26 -0300

    Add in form style for posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -28,9 +28,9 @@ describe 'navigate' do
 
   describe 'new' do
     it 'has a link from the homepage' do
-    visit root_path
-    click_link("new_post_from_nav")
-    expect(page.status_code).to eq(200)
+      visit root_path
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
     end
   end
 
