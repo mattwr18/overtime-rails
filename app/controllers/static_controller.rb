0fd5800665377bec4b2f7f20e7d8a6b7dd9a55b3
Matt Rider <mattwr18@gmail.com>
2017-09-28 14:37:17 -0300

    Implement button to request overtime approval from homepage and make pending audit confirmations visible if there are any pending

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,7 +5,7 @@ class StaticController < ApplicationController
       @recent_audit_items = AuditLog.last(10)
     else
       #TODO fix scope
-      @pending_audit_confirmations = current_user.audit_logs
+      @pending_audit_confirmations = current_user.audit_logs.pending
     end
   end
 end
