d3dc71a2459f9c9e3869b05382f4b4c0375dc56f
Matt Rider <mattwr18@gmail.com>
2017-09-29 09:25:09 -0300

    Build rake task for sms and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
-@user = User.create(email: "test@test.com",
+@employee = Employee.create(email: "test@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: "Daenerys",
                     last_name: "Targaryen",
-                    phone: "48999435231")
+                    phone: "48999843216")
 
-puts "1 user created"
+puts "1 employee created"
 
-AdminUser.create(email: "admin@test.com",
+AdminUser.create(email: "matt.rider@yahoo.com",
                  password: "asdfasdf",
                  password_confirmation: "asdfasdf",
                  first_name: "Arya",
                  last_name: "Stark",
-                 phone: "48999435231")
+                 phone: "48999843216")
 
-puts "1 Amin User created"
+puts "1 Admin user created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",    user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit_logs have been created"
